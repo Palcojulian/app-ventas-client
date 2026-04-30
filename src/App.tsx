d22b8fd7ea7bd8daaad8d7248d36511440f67503
palcojulian <palcojulian@outlook.com>
2026-04-30 16:55:20 -0500

    feat: wrap application in QueryProvider for improved data management

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import Home from './pages/Home';
-
-
+import { QueryProvider } from './providers/QueryProvider';
 
 function App() {
 
-
   return (
-    <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path='/inicio' element={<Home />} />
-          
-        </Routes>
-      </MainLayout>
-    </BrowserRouter>
+    <QueryProvider>
+      <BrowserRouter>
+        <MainLayout>
+          <Routes>
+            <Route path='/inicio' element={<Home />} />
+
+          </Routes>
+        </MainLayout>
+      </BrowserRouter>
+    </QueryProvider>
   )
 }
 
